d373773f0037f7422bcedf3084f39e3f0de0d1d7
katban <grancen@wp.pl>
2015-11-26 10:57:18 +0100

    TIC-47: create subscriberController ((get) all subscribers, (post) add new subscriber, (delete) remove subscriber)

---
+++ _controllers/subscriberController.js
@@ -0,0 +1,38 @@
+var Subscriber = require('../models/subscriber.js');
+
+exports.getSubscribers = function (req, res) {
+  Subscriber.find(function (err, subscribers) {
+      if(err) {
+         res.send(err);
+      }
+      res.json(subscribers);
+  });
+};
+
+exports.postSubscriber = function(req, res) {
+    var subscriber = new Subscriber;
+
+    subscriber.name = req.body.name;
+    subscriber.email = req.body.email;
+
+    subscriber.save(function(err) {
+        if(err) {
+            res.send(err);
+        }
+        res.json({
+            message: 'Your email was add to subscriber\'s list',
+            data: subscriber
+        });
+    });
+};
+
+exports.deleteSubscriber = function (req, res) {
+    Subscriber.findByIdAndRemove(req.params.subscriber_id, function (err) {
+        if (err) {
+            res.send(err);
+        }
+        res.json({
+            message: 'Your mail was delete'
+        });
+    })
+};
